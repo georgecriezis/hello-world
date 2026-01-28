80fcdd42dc0d40020677e85156e6ee0857f994d9
georgecriezis <george.criezis@student.fairfield.edu>
2026-01-28 15:38:23 -0500

    something

--- hello.py
+++ hello.py
@@ -2,3 +2,8 @@
 name = input("Enter your name: ").title()
 clean = " ".join(name.split())
 print(f"Hello, {clean}")
+
+# this code adds two numbers provided by the user
+
+z = float(input("Enter a number for x: ")) + float(input("Enter a number for y: "))
+print(z)
